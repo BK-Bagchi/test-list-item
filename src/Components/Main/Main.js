28338c53135da1165a81d18677830052b31a8276
my name <you@exampl.com>
2020-10-12 01:30:17 +0600

    initial works done

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
@@ -14,7 +13,7 @@ import ShareIcon from '@material-ui/icons/Share';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        maxWidth: 345,
+        maxWidth: 350,
     },
     media: {
         height: 0,
@@ -34,45 +33,48 @@ const useStyles = makeStyles((theme) => ({
 
 const Main = () => {
     const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false);
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
+    const [postData, setPostData] = useState([])
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => {
+            setPostData(data.filter(item => item.id <= 9))
+        })
 
     return (
-        <section className="posts d-flex justify-content-between row">
-            <div className="col-md-4 my-4 d-flex justify-content-center">
-                <Card className={classes.root}>
-                    <CardHeader
-                        avatar={
-                            <Avatar aria-label="recipe" className={classes.avatar}>BK</Avatar>
-                        }
-                        title="Shrimp and Chorizo Paella"
-                        subheader={new Date().toDateString()}
-                    />
-                    <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            This impressive paella is a perfect party dish and a fun meal to cook together with your
-                            guests. Add 1 cup of frozen peas along with the mussels, if you like.
-                        </Typography>
-                    </CardContent>
-                    <CardActions disableSpacing>
-                        <IconButton aria-label="add to favorites">
-                            <ThumbUpAltIcon />
-                        </IconButton>
-                        <IconButton className="mx-auto" onClick={handleExpandClick}>
-                            <small>Comments</small>
-                        </IconButton>
-                        <IconButton aria-label="add to favorites">
-                            <ShareIcon />
-                        </IconButton>
-                    </CardActions>
-                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                        <CardContent> ok</CardContent>
-                    </Collapse>
-                </Card>
-            </div>
+        <section className="posts d-flex justify-content-between row w-100">
+            {
+                postData.map((post, i) => {
+                    const { id, body, title } = post
+                    return (
+                        <div key={id} className="col-md-4 my-4 d-flex justify-content-center">
+                            <Card className={classes.root}>
+                                <CardHeader
+                                    avatar={
+                                        <Avatar aria-label="recipe" className={classes.avatar}>{title[0].toUpperCase()}</Avatar>
+                                    }
+                                    title={title}
+                                    subheader={new Date().toDateString()}
+                                />
+                                <CardContent>
+                                    <Typography variant="body2" color="textSecondary" component="p">{body}</Typography>
+                                </CardContent>
+                                <CardActions disableSpacing>
+                                    <IconButton aria-label="add to favorites">
+                                        <ThumbUpAltIcon />
+                                    </IconButton>
+                                    <IconButton className="mx-auto">
+                                        <small>Comments</small>
+                                    </IconButton>
+                                    <IconButton aria-label="add to favorites">
+                                        <ShareIcon />
+                                    </IconButton>
+                                </CardActions>
+                            </Card>
+                        </div>
+                    )
+                })
+            }
         </section>
     );
 }

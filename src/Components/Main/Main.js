e82ea7f409e565c4cbc68653c370663a8066448d
my name <you@exampl.com>
2020-10-12 12:26:48 +0600

    some style added to make it good looking

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Main.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -10,10 +11,12 @@ import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ShareIcon from '@material-ui/icons/Share';
+import NavBar from './NavBar';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         maxWidth: 350,
+        background: 'linear-gradient(150deg, #34ace0 20%, #576574 80%, #222f3e 20%)'
     },
     media: {
         height: 0,
@@ -42,40 +45,44 @@ const Main = () => {
         })
 
     return (
-        <section className="posts d-flex justify-content-between row w-100">
-            {
-                postData.map((post, i) => {
-                    const { id, body, title } = post
-                    return (
-                        <div key={id} className="col-md-4 my-4 d-flex justify-content-center">
-                            <Card className={classes.root}>
-                                <CardHeader
-                                    avatar={
-                                        <Avatar aria-label="recipe" className={classes.avatar}>{title[0].toUpperCase()}</Avatar>
-                                    }
-                                    title={title}
-                                    subheader={new Date().toDateString()}
-                                />
-                                <CardContent>
-                                    <Typography variant="body2" color="textSecondary" component="p">{body}</Typography>
-                                </CardContent>
-                                <CardActions disableSpacing>
-                                    <IconButton aria-label="add to favorites">
-                                        <ThumbUpAltIcon />
-                                    </IconButton>
-                                    <IconButton className="mx-auto">
-                                        <small>Comments</small>
-                                    </IconButton>
-                                    <IconButton aria-label="add to favorites">
-                                        <ShareIcon />
-                                    </IconButton>
-                                </CardActions>
-                            </Card>
-                        </div>
-                    )
-                })
-            }
-        </section>
+        <>
+            <NavBar />
+            <section className="posts d-flex justify-content-between row w-100">
+                {
+                    postData.map((post, i) => {
+                        const { id, body, title } = post
+                        return (
+                            <div key={id} className="col-md-4 my-4 d-flex justify-content-center">
+                                <Card className={classes.root}>
+                                    <CardHeader
+                                        avatar={
+                                            <Avatar aria-label="recipe" className={classes.avatar}>{title[0].toUpperCase()}</Avatar>
+                                        }
+                                        title={title}
+                                        subheader={new Date().toDateString()}
+                                    />
+                                    <CardContent>
+                                        <Typography variant="body2" color="textSecondary" component="p">{body}</Typography>
+                                    </CardContent>
+                                    <CardActions disableSpacing>
+                                        <IconButton aria-label="add to favorites">
+                                            <ThumbUpAltIcon />
+                                        </IconButton>
+                                        <IconButton className="mx-auto">
+                                            <small>Comments</small>
+                                        </IconButton>
+                                        <IconButton aria-label="add to favorites">
+                                            <ShareIcon />
+                                        </IconButton>
+                                    </CardActions>
+                                </Card>
+                            </div>
+                        )
+                    })
+                }
+            </section>
+            <div className="background"></div>
+        </>
     );
 }
 
